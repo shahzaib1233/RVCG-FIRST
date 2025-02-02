083670684b3689df68c8b93be3aa08ed73b3144b
shahzaib1233 <shahzaibimtiazbt@gmail.com>
2025-02-02 21:01:23 +0500

    test

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -86,27 +86,7 @@ public static function middleware()
     }
 
 
-    public function image(Request $request)
-    {
-        // Validate the image input
-        $request->validate([
-    'image' => 'required|file|mimes:jpeg,png,jpg,gif,pdf,doc,docx,xls,xlsx,zip|max:5120', 
-        ]);
-    
-        // Handle image upload
-        if ($request->hasFile('image')) {
-            $image = $request->file('image');
-            $imageName = time() . '_' . $image->getClientOriginalName(); // Generate unique filename
-            $image->move(public_path('uploads/Listings/Image'), $imageName); // Save in the specified folder
-    
-            return response()->json([
-                'message' => 'Image uploaded successfully',
-                'image_path' => url('uploads/Listings/Image/' . $imageName), // Return the accessible image URL
-            ], 200);
-        }
-    
-        return response()->json(['message' => 'No image uploaded'], 400);
-    }
+   
 
     
 }
