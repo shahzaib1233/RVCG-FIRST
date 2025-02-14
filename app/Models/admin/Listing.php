26f0f0d77d664e3a432696b698e9805df6863cd9
shahzaib1233 <shahzaibimtiazbt@gmail.com>
2025-02-14 17:26:16 +0500

    Test

--- app/Models/admin/Listing.php
+++ app/Models/admin/Listing.php
@@ -14,13 +14,9 @@ class Listing extends Model
 {
     use HasFactory;
     protected $hidden = [
-        'Owner_Full_Name',
-        'Owner_Contact_Number',
-        'Owner_Email_Address',
-        'Owner_Government_ID_Proof',
-        'Owner_Property_Ownership_Proof',
-        'Owner_Ownership_Type',
-        'Owner_Property_Documents'
+        'owner_full_name', 'owner_age', 
+        'owner_contact_number', 'owner_email_address', 'owner_government_id_proof', 
+        'owner_property_ownership_proof', 'owner_ownership_type', 'owner_property_documents'
     ];
     
     protected $fillable = ['id', 'title', 'description','estimated_roi', 'city_id', 'country_id', 'property_type_id', 'property_status_id', 'listing_date', 'price', 'square_foot', 'parking', 'year_built', 'lot_size', 'longitude', 'latitude', 'school_district', 'walkability_score', 'crime_rate','gdrp_agreement', 'roi', 'area','monthly_rent','zip_code','geolocation_coordinates', 'cap_rate', 'address', 'bedrooms', 'bathrooms', 'half_bathrooms', 'arv', 'gross_margin','moa', 'user_id', 'is_featured','is_approved' , 'repair_cost' , 'wholesale_fee' , 'price_per_square_feet' , 'owner_full_name', 'owner_age', 
